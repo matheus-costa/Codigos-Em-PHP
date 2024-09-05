fac39e969cf4595666f771b94322e7e00f61c579
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-09-05 20:06:36 -0300

    Delete Exec02 directory

--- Exec02/observatoriopolicia/inserir.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $local = $_REQUEST['local'];
-    $descricao = $_REQUEST['descricao'];
-    $sql = " insert into ocorrencia values (null, '$local', '$descricao', datetime('now'), null); ";
-    $conexao = new pdo('sqlite:banco');
-    $conexao->exec($sql);
-    unset($conexao);
-    header('Location: lista.php');
--- Exec02/observatoriopolicia/responder.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $id = $_REQUEST['id'];
-    $sql = " update ocorrencia set resposta = datetime('now') where id = '$id'; ";
-    $conexao = new pdo('sqlite:banco');
-    $conexao->exec($sql);
-    unset($conexao);
-    header('Location: lista.php');
